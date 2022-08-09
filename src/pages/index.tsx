ecfb5e2447750d1da93903c15244b3cf6f7c1c97
Samuel Vasconcelos <smvasconcelos11@gmail.com>
2022-08-09 10:59:58 -0300

    configured public page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './home'
 import Search from './search'
 import AddContact from './contact/add'
